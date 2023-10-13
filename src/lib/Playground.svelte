5869edb47f77fd2a45fcff5e3db90116f775b5e4
miksin <contact@miksin.art>
2023-10-13 17:39:15 +0900

    tiles

--- src/lib/Playground.svelte
+++ src/lib/Playground.svelte
@@ -1,18 +1,45 @@
-<div class="playground">(playground)</div>
+<script lang="ts">
+	import Tile from "./Tile.svelte";
+
+	const tileNumbers = Array.from(Array(16).keys());
+	const items = [2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048];
+
+	const numToX = (n: number) => n % 4;
+	const numToY = (n: number) => Math.floor(n / 4);
+</script>
+
+<div class="playground">
+	{#each tileNumbers as tileNumber}
+		<div class="container" style:--x={numToX(tileNumber)} style:--y={numToY(tileNumber)}>
+			<Tile num={items[tileNumber % items.length]} />
+		</div>
+	{/each}
+</div>
 
 <style>
-	.playground {
-		width: var(--playground-size-md);
-		height: var(--playground-size-md);
+	.container {
+		position: absolute;
+		left: calc(var(--x) * 25%);
+		top: calc(var(--y) * 25%);
+		width: 25%;
+		height: 25%;
+		border: 4px solid transparent;
+	}
 
-		background-color: var(--base-color-3);
+	.playground {
+		width: 512px;
+		height: 512px;
+
+		position: relative;
+		background-color: var(--base-color-2);
 		border-radius: 6px;
+		border: 4px solid var(--base-color-2);
 	}
 
 	@media screen and (max-width: 512px) {
 		.playground {
-			width: var(--playground-size-sm);
-			height: var(--playground-size-sm);
+			width: 300px;
+			height: 300px;
 		}
 	}
 </style>
---
+++ src/lib/Tile.svelte
@@ -0,0 +1,45 @@
+<script lang="ts">
+    export let num: number | undefined;
+    const level = num ? Math.log2(num) : undefined;
+</script>
+
+<div class={`tile ${level ? `--${level}` : ''}`}>
+    <h6>{num}</h6>
+</div>
+
+<style lang="scss">
+    .tile {
+        color: white;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        border-radius: 4px;
+        font-size: 64px;
+        width: 100%;
+        height: 100%;
+
+        @for $i from 1 through 11 {
+            &.--#{$i} {
+                background-color: var(--tile-color-#{$i});
+            }
+        }
+
+        @for $i from 1 through 3 {
+            &.--#{$i} {
+                color: var(--base-color-4);
+            }
+        }
+
+        @for $i from 7 through 9 {
+            &.--#{$i} {
+                font-size: 48px;
+            }
+        }
+
+        @for $i from 10 through 11 {
+            &.--#{$i} {
+                font-size: 36px;
+            }
+        }
+    }
+</style>
